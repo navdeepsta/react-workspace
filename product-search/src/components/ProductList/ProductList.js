1b5bb4717a2bc7952c426d87bce2ce7f055fffa7
navdeep <navdeepst.1987@gmail.com>
2023-11-01 11:01:29 +1100

    Move products data from ProductList.js to a new file products.js

--- product-search/src/components/ProductList/ProductList.js
+++ product-search/src/components/ProductList/ProductList.js
@@ -2,80 +2,10 @@ import { Component } from 'react';
 import Product from '../Product/Product';
 import Button from '../Button/Button';
 import './ProductList.css';
-
-const products = [
-  {
-    name: "Leather Jacket",
-    category: "jackets",
-    description:
-      "Whether it's to protect from wind or just to look super cool, this leather jacket has you covered.",
-    price: 400,
-  },
-  {
-    name: "Wool cardigan",
-    category: "jackets",
-    description:
-      "Beautifully warm and soft, this cardigan will make you feel cosy on a cold day.",
-    price: 80,
-  },
-  {
-    name: "Striped business shirt",
-    category: "shirts",
-    description:
-      "No ironing necessary to look professional every day with this striped shirt.",
-    price: 50,
-  },
-  {
-    name: "Short-sleeved polo shirt",
-    category: "shirts",
-    description: "The best shirt you can get for that business-casual look.",
-    price: 30,
-  },
-  {
-    name: "Plain business shirt",
-    category: "shirts",
-    description:
-      "No ironing necessary to look professional every day with this plain business shirt.",
-    price: 50,
-  },
-  {
-    name: "Suit Jacket",
-    category: "jackets",
-    description: "Wear it with jeans or suit pants, it works with both!",
-    price: 120,
-  },
-  {
-    name: "Suit Trousers",
-    category: "pants",
-    description:
-      "Get 5 of these and you've got pants for every day of the week.",
-    price: 100,
-  },
-  {
-    name: "Denim Jeans",
-    category: "pants",
-    description:
-      "A timeless classic, these denim jeans will never go out of style.",
-    price: 80,
-  },
-  {
-    name: "Pencil Skirt",
-    category: "skirts",
-    description:
-      "A classy work-ready skirt that will make you feel like a million bucks.",
-    price: 100,
-  },
-  {
-    name: "Cotton flowy skirt",
-    category: "skirts",
-    description:
-      "For those warm summer days when you just need to feel the breeze on your legs.",
-    price: 45,
-  },
-];
+import products from '../../data/products';
 
 export default class ProductList extends Component {
-
+  
   state = { products: products, search: '' };
   
   filterProducts = (...categories) => {
